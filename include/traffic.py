5214d7add2dcaaf4e1ea40b32ce9b320eb1c1ede
AdiKsOnDev <adilalizade13@gmail.com>
2025-02-21 16:14:23 +0400

    fix: Catch any error that occurs while running a file

--- include/traffic.py
+++ include/traffic.py
@@ -15,7 +15,7 @@ def run_executable(executable_path):
     try:
         include_logger.debug(f"Running executable: {executable_path}")
         subprocess.run(executable_path, check=True)
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         include_logger.error(f"Error running {executable_path}", exc_info=True)
 
 def capture_traffic(interface, output_file, stop_event, timeout=10):
