c55eb9bf4e2b052f5cb06777041c227b8f1fe549
AdiKsOnDev <adilalizade13@gmail.com>
2025-03-06 16:56:59 +0400

    feat: Function for analysis can also extract detailed packet information

--- include/utils.py
+++ include/utils.py
@@ -13,15 +13,31 @@ def write_packets_to_csv(filename, packets, protocol, csv_writer):
         csv_writer (csv.writer): A CSV writer object to write the data to the file.
     """
     for packet in packets:
-        source_ip = packet.ip.src if 'IP' in packet else 'N/A'
-        destination_ip = packet.ip.dst if 'IP' in packet else 'N/A'
-        details = ''
-
         if protocol == 'DNS':
-            details = packet.dns.qry_name if 'DNS' in packet and hasattr(packet.dns, 'qry_name') else 'N/A'
+            details = {
+                'Query Name': packet.dns.qry_name if hasattr(packet.dns, 'qry_name') else 'N/A',
+                'Response Flags': packet.dns.flags if hasattr(packet.dns, 'flags') else 'N/A',
+                'Time-to-Live': packet.dns.time_to_live if hasattr(packet.dns, 'time_to_live') else 'N/A'
+            }
         elif protocol == 'HTTP':
-            details = packet.http.request_uri if 'HTTP' in packet and hasattr(packet.http, 'request_uri') else 'N/A'
+            details = {
+                'Hostname': packet.http.host if hasattr(packet.http, 'host') else 'N/A',
+                'Referrer': packet.http.referer if hasattr(packet.http, 'referer') else 'N/A',
+                'Cookie': packet.http.cookie if hasattr(packet.http, 'cookie') else 'N/A',
+                'User Agent': packet.http.user_agent if hasattr(packet.http, 'user_agent') else 'N/A',
+                'Content Type': packet.http.content_type if hasattr(packet.http, 'content_type') else 'N/A'
+            }
         elif protocol == 'SSL/TLS':
-            details = packet.ssl.handshake_type if 'SSL' in packet and hasattr(packet.ssl, 'handshake_type') else 'N/A'
+            details = {
+                'Server Name': packet.ssl.server_name if hasattr(packet.ssl, 'server_name') else 'N/A',
+                'SSL Version': packet.ssl.version if hasattr(packet.ssl, 'version') else 'N/A',
+                'Certificate Expiry': packet.ssl.handshake_certificate_expiration if hasattr(packet.ssl, 'handshake_certificate_expiration') else 'N/A'
+            }
 
-        csv_writer.writerow([filename, protocol, source_ip, destination_ip, details])
+        csv_writer.writerow([
+            executable_name,
+            protocol,
+            packet.ip.src if hasattr(packet, 'ip') else 'N/A',
+            packet.ip.dst if hasattr(packet, 'ip') else 'N/A',
+            details
+        ])
